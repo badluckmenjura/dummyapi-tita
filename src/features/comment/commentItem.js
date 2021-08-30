8063b18f06886d052873ef196985101c77a39931
dany <danymenjura@gmail.com>
2021-08-29 22:27:40 -0500

    Add files via upload

--- src/features/comment/commentItem.js
+++ src/features/comment/commentItem.js
@@ -4,7 +4,7 @@ import { AuthorHeader } from '../authorHeader/AuthorHeader';
 export function CommentItem(props)  {
 
   return (
-    <div>
+    <div className='comment'>
       <AuthorHeader {...props.owner} publishDate={props.publishDate}></AuthorHeader>
       <div className="content">
         {props.message}
